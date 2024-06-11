34553d3bd4db11c25ed86cfbc42665dc1dda8015
rachitkatariyaa <raachitkumar2953@gmail.com>
2024-06-11 14:54:34 +0530

    adding files

---
+++ 121. Best Time to Buy and Sell Stock/MaxProfit.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class MaxProfit {
+
+    public static void maxProfit(int[] prices) {
+        int n = prices.length;
+        int[] bestBuy = new int[n];
+        Arrays.fill(bestBuy, Integer.MAX_VALUE);
+
+        for (int i = 1; i < n; i++) {
+            bestBuy[i] = Math.min(bestBuy[i - 1], prices[i - 1]);
+        }
+
+        int maxProfit = 0;
+        for (int i = 0; i < n; i++) {
+            int currProfit = prices[i] - bestBuy[i];
+            maxProfit = Math.max(maxProfit, currProfit);
+        }
+
+        System.out.println("Max profit: " + maxProfit);
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {7, 1, 5, 3, 6, 4};
+        maxProfit(prices);
+    }
+}
